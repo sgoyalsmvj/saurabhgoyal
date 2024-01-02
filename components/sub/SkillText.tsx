f15927a8acac84e88294317213f2979df91b63af
sgoyalsmvj <sgoyalsmvj@gmail.com>
2024-01-02 19:08:19 +0530

    little change.

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -17,7 +17,7 @@ const SkillText = () => {
       >
         <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
         <h1 className="Welcome-text text-[13px]">
-          Think better with Nextjs 14
+          Build better with Me.
         </h1>
       </motion.div>
       <motion.div
